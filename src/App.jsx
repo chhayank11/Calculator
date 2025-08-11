baf8825874973460ac87e0e8c6de94e193e7f7b4
Chhayank <cdixit1313@gmail.com>
2025-08-12 00:44:55 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./components/button/Button";
+import { evaluate } from "mathjs";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [result, setResult] = useState("");
+  const [text, setText] = useState("");
 
+  const clickHandler = (item) => {
+    setText((prev) => prev + item);
+  };
+  const calculate = () => {
+    if ("+-*/".includes(text.charAt(text.length - 1)) || text.length === 0) {
+      setResult("Error");
+      return;
+    }
+
+    setResult(evaluate(text));
+  };
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>React Calculator</h1>
+      <input type="text" value={text} disabled />
+      <p>{result}</p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button text={"7"} clickHandler={clickHandler} />
+        <Button text={"8"} clickHandler={clickHandler} />
+        <Button text={"9"} clickHandler={clickHandler} />
+        <Button text={"+"} clickHandler={clickHandler} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div>
+        <Button text={"4"} clickHandler={clickHandler} />
+        <Button text={"5"} clickHandler={clickHandler} />
+        <Button text={"6"} clickHandler={clickHandler} />
+        <Button text={"-"} clickHandler={clickHandler} />
+      </div>
+      <div>
+        <Button text={"1"} clickHandler={clickHandler} />
+        <Button text={"2"} clickHandler={clickHandler} />
+        <Button text={"3"} clickHandler={clickHandler} />
+        <Button text={"*"} clickHandler={clickHandler} />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setText("");
+            setResult("");
+          }}
+        >
+          C
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <Button text={"0"} clickHandler={clickHandler} />
+        <button onClick={calculate}>=</button>
 
-export default App
+        <Button text={"/"} clickHandler={clickHandler} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
